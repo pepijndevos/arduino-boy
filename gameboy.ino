d05a3d828a5d34fc7355a578835bfed12e396854
Bede Kelly <bedekelly@users.noreply.github.com>
2015-02-24 12:23:21 +0000

    Fix typo(s)
    LENGHT -> LENGTH

--- gameboy.ino
+++ gameboy.ino
@@ -168,7 +168,7 @@ byte handleIncomingByte(byte in) {
       }
       INPUT_BLOCK[counter] = in;
       counter++;
-      if(counter == PLAYER_LENGHT) {
+      if(counter == PLAYER_LENGTH) {
         trade_centre_state = SENDING_PATCH_DATA;
       }
     } else if(trade_centre_state == SENDING_PATCH_DATA && in == 0xFD) {
