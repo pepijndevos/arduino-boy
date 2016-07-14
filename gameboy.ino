cb198c28ccb3059ad648009cd5cf6fe72425afbd
Pepijn de Vos <pepijndevos@gmail.com>
2016-07-14 11:29:57 +0200

    Initial Attempt at Pokemon Go

--- gameboy.ino
+++ gameboy.ino
@@ -1,10 +1,15 @@
 #include <EEPROM.h>
+#include <SPI.h>
 #include "pokemon.h"
+#include "pokemon_constants.h"
 #include "output.h"
+#include <Adafruit_GPS.h>
+#include <math.h>
 
-#define MOSI_ 51
-#define MISO_ 50
-#define SCLK_ 52
+#define GPSSerial Serial1
+Adafruit_GPS GPS(&GPSSerial);
+#define GPSECHO false
+IntervalTimer gpstimer;
 
 connection_state_t connection_state = NOT_CONNECTED;
 trade_centre_state_t trade_centre_state = INIT;
@@ -12,45 +17,100 @@ int counter = 0;
 
 int trade_pokemon = -1;
 
-uint8_t transferByte(uint8_t out) {
-    uint8_t in = 0;
-    for(int i = 8; i; i--) {
-        digitalWrite(MOSI_, out & 0x80 ? HIGH : LOW);
-        out <<= 1;
-        
-        digitalWrite(SCLK_, LOW);
-        delayMicroseconds(500);
-        
-        digitalWrite(SCLK_, HIGH);
-        delayMicroseconds(500);
+#define EEPROM_DIST_ADDR 86
+#define MIN_WALK 20
+#define MAX_WALK 100
+#define ENCOUNTER_RANDOMNESS 10
+float distance_walked = 0;
+float next_encounter = 0;
+unsigned int encounter_idx = ENCOUNTER_RANDOMNESS;
 
-        in <<= 1;
-        in |= digitalRead(MISO_);
-    }
-    return in;
+uint8_t transferByte(uint8_t out) {
+  return SPI.transfer(out);
 }
 
 void setup() {
-    Serial.begin(115200);
-    pinMode(SCLK_, OUTPUT);
-    pinMode(MISO_, INPUT);
-    pinMode(MOSI_, OUTPUT);
-    
-    Serial.print("hello world\n");
-    
-    digitalWrite(MOSI_, LOW);
-    digitalWrite(SCLK_, HIGH);
+  Serial.begin(115200);
+  Serial.print("hello world\n");
+
+  SPI.begin();
+  SPI.beginTransaction(SPISettings(8000, MSBFIRST, SPI_MODE3));
+
+  GPS.begin(9600);
+  GPS.sendCommand(PMTK_ENABLE_SBAS);
+  GPS.sendCommand(PMTK_SET_NMEA_OUTPUT_RMCONLY);
+  GPS.sendCommand(PMTK_SET_NMEA_UPDATE_100_MILLIHERTZ); // 10s update rate
+  //GPS.sendCommand(PGCMD_ANTENNA);
+  gpstimer.begin(gpsread, 1000);  // blinkLED to run every 0.15 seconds
+
+  //EEPROM.put(EEPROM_DIST_ADDR, distance_walked);
+  EEPROM.get(EEPROM_DIST_ADDR, distance_walked);
+  next_encounter = distance_walked + random(MIN_WALK, MAX_WALK);
+}
+
+void gpsread(void) {
+  GPS.read();
 }
 
 uint8_t next = PKMN_MASTER;
 void loop() {
+  if(connection_state!=POKEMON_GO) {
     uint8_t in = transferByte(next);
     next = handleIncomingByte(in);
-    Serial.print(in, HEX);
-    Serial.print(" ");
-    Serial.print(next, HEX);
-    Serial.print("\n");
+    //Serial.print(in, HEX);
+    //Serial.print(" ");
+    //Serial.print(next, HEX);
+    //Serial.print("\n");
     delay(100);
+  } else {
+    pokemon_go();
+  }
+  if (GPS.newNMEAreceived()) {
+    if (!GPS.parse(GPS.lastNMEA()))   // this also sets the newNMEAreceived() flag to false
+      return;  // we can fail to parse a sentence in which case we should just wait for another
+
+    if (GPS.fix) {
+      if(GPS.speed > 1 && GPS.speed < 20) // Reasonable walking/running speeds (?)
+        distance_walked += GPS.speed*5.144; // knots to meters per 10 seconds
+      Serial.print("Location: ");
+      Serial.print(GPS.latitudeDegrees, 4); Serial.print(GPS.lat);
+      Serial.print(", "); 
+      Serial.print(GPS.longitudeDegrees, 4); Serial.println(GPS.lon);
+      
+      Serial.print("Speed (knots): "); Serial.println(GPS.speed);
+      Serial.print("Angle: "); Serial.println(GPS.angle);
+      Serial.print("Altitude: "); Serial.println(GPS.altitude);
+      Serial.print("Satellites: "); Serial.println((int)GPS.satellites);
+      Serial.print("Distance walked(meter): "); Serial.println(distance_walked);
+    }
+  }
+}
+ 
+#define R 6371
+#define TO_RAD (3.1415926536 / 180)
+double dist(double th1, double ph1, double th2, double ph2)
+{
+  double dx, dy, dz;
+  ph1 -= ph2;
+  ph1 *= TO_RAD, th1 *= TO_RAD, th2 *= TO_RAD;
+ 
+  dz = sin(th1) - sin(th2);
+  dx = cos(ph1) * cos(th1) - cos(th2);
+  dy = sin(ph1) * cos(th1);
+  return asin(sqrt(dx * dx + dy * dy + dz * dz) / 2) * 2 * R;
+}
+
+void pokemon_go(void) {
+  if(distance_walked > next_encounter) {
+      next_encounter = distance_walked + random(MIN_WALK, MAX_WALK);
+      int id = encounter_idx + random(-ENCOUNTER_RANDOMNESS, ENCOUNTER_RANDOMNESS);
+      wild_pokemon_t pkm = wild_pokemon[id];
+      Serial.println(pkm.species, HEX); 
+      Serial.println(pkm.level); 
+      transferByte(pkm.species);
+      delay(100);
+      transferByte(pkm.level);
+  }
 }
 
 byte handleIncomingByte(byte in) {
@@ -74,6 +134,8 @@ byte handleIncomingByte(byte in) {
       connection_state = TRADE_CENTRE;
     else if(in == PKMN_COLOSSEUM)
       connection_state = COLOSSEUM;
+    else if(in == PKMN_GO)
+      connection_state = POKEMON_GO;
     else if(in == PKMN_BREAK_LINK || in == PKMN_MASTER) {
       connection_state = NOT_CONNECTED;
       send = PKMN_BREAK_LINK;
--- pokemon.h
+++ pokemon.h
@@ -5,7 +5,8 @@ typedef enum {
 	NOT_CONNECTED,
 	CONNECTED,
 	TRADE_CENTRE,
-	COLOSSEUM
+	COLOSSEUM,
+  POKEMON_GO
 } connection_state_t;
 
 typedef enum {
@@ -29,9 +30,11 @@ typedef unsigned char byte;
 #define ITEM_1_HIGHLIGHTED				0xD0
 #define ITEM_2_HIGHLIGHTED				0xD1
 #define ITEM_3_HIGHLIGHTED				0xD2
+#define ITEM_4_HIGHLIGHTED        0xD3
 #define ITEM_1_SELECTED					0xD4
 #define ITEM_2_SELECTED					0xD5
 #define ITEM_3_SELECTED					0xD6
+#define ITEM_4_SELECTED         0xD7
 
 #define PKMN_MASTER						0x01
 #define PKMN_SLAVE						0x02
@@ -42,7 +45,8 @@ typedef unsigned char byte;
 
 #define PKMN_TRADE_CENTRE				ITEM_1_SELECTED
 #define PKMN_COLOSSEUM					ITEM_2_SELECTED
-#define PKMN_BREAK_LINK					ITEM_3_SELECTED
+#define PKMN_GO                 ITEM_3_SELECTED
+#define PKMN_BREAK_LINK         ITEM_4_SELECTED
 
 #define TRADE_CENTRE_WAIT				0xFD
 
